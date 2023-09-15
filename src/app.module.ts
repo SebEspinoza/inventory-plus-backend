77aa80f672e5ddb2e9be10b20889a43b519f4489
SebEspinoza <seba.espinozal@duocuc.cl>
2023-09-14 21:39:53 -0300

    separated products endpoint from main app files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Product, ProductSchema } from './schemas/product.schema';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './users/user.module';
+import { ProductModule } from './products/product.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_URI), MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]), UserModule],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_URI), ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
