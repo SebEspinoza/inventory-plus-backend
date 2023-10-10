703a51a61b75b834673e4d55e8b92080250cc183
SebEspinoza <seba.espinozal@duocuc.cl>
2023-10-09 23:03:03 -0300

    changed json limit size

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '50mb' })); // Increase the limit of the body size
   app.enableCors(); // Enable CORS for all origins
   app.use(cookieParser()); // Use cookie parser
   await app.listen(3000);
