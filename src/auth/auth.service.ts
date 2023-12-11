5abd7c5f5583fc483dc61fde1847b2adc6856289
SebEspinoza <seba.espinozal@duocuc.cl>
2023-12-10 21:49:25 -0300

    correct error handling test 1

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,11 @@
-import { Injectable, Res, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/schemas/user.schema';
 import * as bcrypt from 'bcrypt';
-import { JwtService, JwtSignOptions } from '@nestjs/jwt';
+import { JwtService } from '@nestjs/jwt';
 import { SignUpDto } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
-
-
-
 
 @Injectable()
 export class AuthService {
@@ -40,17 +37,24 @@ export class AuthService {
         try {
             const { email, password } = loginDto;
             const user = await this.userModel.findOne({ email });
+
             if (!user) {
-                throw new UnauthorizedException('Correo o Contraseña Incorrecta');
+                throw new NotFoundException('Usuario Invalido');
             }
+
             const isPasswordMatch = await bcrypt.compare(password, user.password);
+
             if (!isPasswordMatch) {
-                throw new UnauthorizedException('Correo o Contraseña Incorrecta');
+                throw new UnauthorizedException('Credenciales Invalidas');
             }
+
             const refresh_token = this.jwtService.sign({ id: user._id, username: user.username });
             return { token: refresh_token, success: true, type: user.role, data: user.username };
-        } catch {
-            throw new UnauthorizedException('Correo o Contraseña Incorrecta');
+        } catch (error) {
+            if (error instanceof UnauthorizedException || error instanceof NotFoundException) {
+                throw error;
+            }
+            throw new UnauthorizedException('Credenciales Invalidas');
         }
     }
-}  
+}
